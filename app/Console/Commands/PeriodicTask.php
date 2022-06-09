49c87c9f0897879870f4f6243e48d3eeac164f4d
Mwansasquared <mwansasquared9@gmail.com>
2022-06-09 03:21:34 +0200

    first commit

---
+++ app/Console/Commands/PeriodicTask.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PeriodicTask extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+
+    public function _construct() {
+        parent::__construct();
+    }
+    public function handle()
+    {
+        return 0;
+    }
+}
